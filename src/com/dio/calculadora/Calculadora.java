03bf8cc8b28d8ef78af7992943cbb25caaa7de60
thiaguinhobyra <byraexpert@gmail.com>
2021-09-29 17:41:18 -0300

    6

--- src/com/dio/calculadora/Calculadora.java
+++ src/com/dio/calculadora/Calculadora.java
@@ -17,7 +17,7 @@ public class Calculadora {
 		int soma = soma(a,b);
 		int subtracao = subtracao(a,b);
 		int multiplicacao = multiplicacao(a,b);
-		int divisao = divisao(a,b);
+		double divisao = divisao(a,b);
 		
 		System.out.println(soma);
 		System.out.println(subtracao);
@@ -35,7 +35,7 @@ public class Calculadora {
 	public static int multiplicacao(int a, int b) {
 		return a * b;
 	}
-	public static int divisao(int a, int b) {
+	public static double divisao(int a, int b) {
 		return a / b;
 	}
 }
